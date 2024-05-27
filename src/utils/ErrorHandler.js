b205aab7ff43b986dd5cf9b06c125c92ac523bae
shivank <shivank.pasi@somaiya.edu>
2024-05-27 18:30:14 +0530

    update folder structer

--- src/utils/ErrorHandler.js
+++
@@ -1,9 +0,0 @@
-// Errorhandler class to raise any error
-// it will invoke errormiddleware
-class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-export default ErrorHandler;
